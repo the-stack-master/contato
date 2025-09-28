8291196b8df339516f06cd0091c4c7bb57bd0339
Muhammed <muhammedbilal.dev@gmail.com>
2025-09-28 14:46:45 +0530

    lint error fix

--- src/app/(main)/features/page.tsx
+++ src/app/(main)/features/page.tsx
@@ -187,7 +187,7 @@ const FeaturesPage = () => {
         className="max-w-7xl mx-auto px-6 py-20 bg-gray-50 rounded-2xl shadow-sm space-y-12"
       >
         <h2 className="text-4xl font-extrabold text-center text-[#f15A24] mb-16">
-          What's New?
+          What&apos;s New?
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto">
           {keyFeatures.map((feature) => (
